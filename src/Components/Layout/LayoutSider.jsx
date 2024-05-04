1c74cdff32f85ce3a0f65100726b4177fbcd2f0c
Sweetreams <pashasurov12345@gmail.com>
2024-05-04 21:03:39 +0700

    d

--- src/Components/Layout/LayoutSider.jsx
+++ src/Components/Layout/LayoutSider.jsx
@@ -2,12 +2,13 @@ import { Layout, Menu } from 'antd';
 import React from 'react'
 import { items } from '../../data'
 import imgLogo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom';
 
 const LayoutSider = () => {
     return (
         <Layout.Sider >
             <div className="demo-logo-vertical" style={{display: 'flex', justifyContent:'center', paddingTop: 10}}>
-                <a href='/mainpage'><img src={imgLogo} alt="sad" /></a>
+                <Link href='/mainpage'><img src={imgLogo} alt="sad" /></Link>
             </div>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
         </Layout.Sider>
